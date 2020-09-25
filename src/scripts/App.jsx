e30dd9d98de11699d8dfecb6ac08267996807c7c
Aniket Chowdhury <aniket_chowdhury@hotmail.com>
2020-09-25 12:24:03 +0530

    feat: redirect css

--- src/scripts/App.jsx
+++ src/scripts/App.jsx
@@ -47,6 +47,7 @@ class App extends React.Component {
 		video.setAttribute('controls', 'controls')
 		video.setAttribute('height',480)
 		video.setAttribute('width',640)
+		video.setAttribute('preload','none')
 		video.src = window.URL.createObjectURL(superBuffer)
 		console.log(video)
 		video.play()

f5f36f10f21506b1f7512431cc5df144c2eeb90b
Aniket Chowdhury <aniket_chowdhury@hotmail.com>
2020-09-25 10:58:06 +0530

    feat: inital commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './main.css'
+import './lucario.css'
 import './styles/index.scss'
 import App from './scripts/App'
 import * as serviceWorker from './serviceWorker'

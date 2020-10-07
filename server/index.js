7c53991c622479d2cf4dd157c812a85cbca28dd9
Aniket Chowdhury <aniket_chowdhury@hotmail.com>
2020-10-07 13:40:12 +0530

    feat: server works

--- server/index.js
+++ server/index.js
@@ -27,6 +27,30 @@ var type = upload.single('file')
 
 app.use('/videos', express.static(__dirname + '/videos/'))
 
+const fs = require('fs')
+
+app.get('/list', (req, res) => {
+	let sendFeed = []
+	let files = fs.readdirSync(__dirname + '/videos/')
+	let i = 0
+	files.forEach((file) => {
+		console.log('http://localhost:8080/videos/' + file)
+		let obj = {}
+		obj['id'] = i
+
+		obj['username'] = '@' + file.split('-')[0]
+		obj['tags'] = '#tag #tag #tag #tag #tagv #tag'
+		obj['music'] = 'hello'
+		obj['likes'] = 9444
+		obj['comments'] = 6340
+
+		obj['uri'] = 'http://localhost:8080/videos/' + file
+		sendFeed.push(obj)
+		i += 1
+	})
+	res.json(sendFeed)
+})
+
 app.post('/', type, (req, res) => {
 	req.on('data', (data) => {
 		console.log(data.toString())
